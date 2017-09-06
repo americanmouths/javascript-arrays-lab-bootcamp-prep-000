db046a58f21718f1d084461a1fdaf63cbf95ed05
Kayla Hansen <shiverr@gmail.com>
2017-09-06 01:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,7 @@ function destructivelyRemoveFirstKitten(name) {
 function appendKitten(name) {
   return [...kittens, "Broom"];
 }
+
+function prependKitten(name) {
+  return ["Arnold", ...kittens];
+}
